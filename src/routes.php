e1abc91fa9724aeb2ba860c6f81a8428301fd29f
Laslo Horvath <bosske1@gmail.com>
2018-12-23 14:57:12 +0100

    added image stuff

--- src/routes.php
+++ src/routes.php
@@ -14,6 +14,28 @@ $app->get('/matches', function (Request $request, Response $response, array $arg
 
     $matches = $this->matchService->firstLoadMatches($dateFrom, $dateTo)->thenSortMatches()->finallyReturnSortedMatches();
 
+    $guzzle = $this->guzzle;
+    $matchesUrl = $this->settings['matchesUrl'];
+
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', ['matches' => $matches]);
+    return $this->renderer->render(
+        $response,
+        'index.phtml',
+        [
+            'matches' => $matches,
+            'urlBase' => $matchesUrl,
+            'imageRenderer' => function(\Widget\Models\Team $team) use ($guzzle, $matchesUrl) {
+                $imageHtml = "";
+                try {
+                    $imageDataRaw = $guzzle->get($matchesUrl . $team->getImageUrl());
+                    $imageData = json_decode($imageDataRaw->getBody(), true);
+                    $imageHtml = "<img src='data:" . $imageData['data']['mime'] . ";base64," . $imageData['data']['src'] . "' />";
+                } catch (\GuzzleHttp\Exception\ClientException $clientException) {
+                    // handle or something:
+                }
+
+                return $imageHtml;
+            }
+        ]
+    );
 });
